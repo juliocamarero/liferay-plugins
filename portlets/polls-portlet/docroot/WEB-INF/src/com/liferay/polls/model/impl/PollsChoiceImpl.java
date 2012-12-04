b1bacb9b4a12b5cf770a23bbde831bc7fc5a13be
Juan Fernández <juan.fernandez@liferay.com>
2012-12-05 00:14:54 +0100

    LPS-30029 Copy files from the core

--- portlets/polls-portlet/docroot/WEB-INF/src/com/liferay/polls/model/impl/PollsChoiceImpl.java
+++ portlets/polls-portlet/docroot/WEB-INF/src/com/liferay/polls/model/impl/PollsChoiceImpl.java
@@ -14,21 +14,19 @@
 
 package com.liferay.polls.model.impl;
 
+import com.liferay.polls.service.PollsVoteLocalServiceUtil;
+import com.liferay.portal.kernel.exception.SystemException;
+
 /**
- * The extended model implementation for the PollsChoice service. Represents a row in the &quot;Polls_PollsChoice&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.polls.model.PollsChoice} interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 public class PollsChoiceImpl extends PollsChoiceBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a polls choice model instance should use the {@link com.liferay.polls.model.PollsChoice} interface instead.
-	 */
+
 	public PollsChoiceImpl() {
 	}
+
+	public int getVotesCount() throws SystemException {
+		return PollsVoteLocalServiceUtil.getChoiceVotesCount(getChoiceId());
+	}
+
 }
